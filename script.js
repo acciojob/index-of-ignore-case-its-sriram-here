9056052f99e30beaf2ac79d973c38e73205fd6af
SRIRAM <srirammurugesan1807@gmail.com>
2025-06-02 11:15:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	 if (s2 === "") return 0;
+
+  return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
